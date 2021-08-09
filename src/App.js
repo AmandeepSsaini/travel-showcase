12744b4252d98d0759463836166a87be4176d994
AmandeepSsaini <amandeepsingh.saini@wsu.edu>
2021-08-08 21:59:12 -0500

    initial changes to develop

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />,
+      <h1> This is the Hello world</h1>
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+function Navbar() {
+    const [click, setClick] = useState(false)
+    return (
+        <>
+            <nav className="Navbar">
+                <div className="navbar-container">
+
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
